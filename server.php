aad58b54b15ba98384c2ef8f5c0d134cd99c27aa
Manas <manaspa@yahoo-inc.com>
2014-09-20 21:36:48 -0700

    [Learning] Autocomplete

--- server.php
+++ server.php
@@ -2,41 +2,18 @@
 //echo "My first PHP script!";
 
 $con=mysqli_connect("manastestsite.zxq.net","923790_manas","webserver123","manastestsite_zxq_testtable");
+
 // Check connection
-//if (mysqli_connect_errno()) {
-//  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-//}
-//else{
-//	echo "successfully connected to database! :) \n\n";
-//}
-/*
-$val = mysqli_query($con, 'SELECT * FROM Persons');
-if($val){
-	$sql="CREATE TABLE Persons(FirstName CHAR(30),LastName CHAR(30),Age INT)";
-	if (mysqli_query($con,$sql)) {
-  		echo "Table persons created successfully";
-	} else {
-  		echo "Error creating table: " . mysqli_error($con);
-	}
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-
-mysqli_query($con,"INSERT INTO Persons (FirstName, LastName, Age) VALUES ('Peter', 'Griffin',35)");
-
-mysqli_query($con,"INSERT INTO Persons (FirstName, LastName, Age) VALUES ('Glenn', 'Quagmire',33)"); */
 
 $result = mysqli_query($con,"SELECT FirstName FROM Persons");
-echo '<select name="formGender">';
-echo '<option value="">Select...</option>';
 $i = 0;
 while($row = mysqli_fetch_array($result)) {
-  echo '<option value=';
-  echo $i;
-  echo '>';
   echo $row['FirstName'];
-  echo '</option>';
-  $i++;
+  echo ',';
 }
-echo '</select>';
 
 mysqli_close($con);
 
